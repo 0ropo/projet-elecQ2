a522417a66eff25df8cab1ae9cd8cda95c353605
hNeut <hugo.neut04@gmail.com>
2024-03-16 16:25:01 +0200

    photiresistor marche seuls.

--- photoresitor.py
+++ photoresitor.py
@@ -1,8 +1,18 @@
 from machine import ADC, Pin , PWM 
 import utime
 A0= ADC(27)
-pwm0 = PWM(Pin(26))      # création d'un objet PWM lié à la broche 0
+pwm0 = PWM(Pin(26))      
 pwm0.freq(100)
 
 while True:
-    pass
+    digital_value = A0.read_u16()     
+    
+    volt=3.3*(digital_value/65535)
+    
+    
+
+    pwm0 = PWM(Pin(26))      
+    pwm0.duty_u16( int((1/volt) *(65535/50) )) 
+        
+        
+    utime.sleep_ms(100)
